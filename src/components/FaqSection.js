cdfc2f1e8afee4738d7662176078489e57798cb1
camille <camillecalas@hotmail.fr>
2024-04-16 11:50:49 +0200

    toggle faq

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,4 +1,7 @@
-import React, {useState} from 'react'
+import React from 'react'
+import Toggle from './Toggle'
+
+import { LayoutGroup } from 'framer-motion'
 
 import styled from 'styled-components'
 import { BasicLayout } from '../styles'
@@ -7,41 +10,35 @@ const FaqSection = () => {
   return (
 	<Faq>
 		<h2>Any questions ?  <span>FAQ</span></h2>
-		<div className="question">
-			<h4>How do I start ?</h4>
-			<div className="answer">
-				<p>Quisque vel congue tortor. </p>
-				<p>Nullam laoreet gravida aliquam. Quisque rutrum arcu eu sapien volutpat venenatis. Donec eu imperdiet metus. </p>
-			</div>
-			<div className="faq-line"></div>
-		</div>
+		<LayoutGroup>
+			<Toggle title="How do I start ?">
+					<div className="answer">
+						<p>Quisque vel congue tortor. </p>
+						<p>Nullam laoreet gravida aliquam. Quisque rutrum arcu eu sapien volutpat venenatis. Donec eu imperdiet metus. </p>
+					</div>
+			</Toggle>
 
-		<div className="question">
-			<h4>Daily schedule</h4>
-			<div className="answer">
-				<p>Donec ultricies nisi justo, at auctor massa</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac odio congue, aliquet lectus id, pulvinar ex. Vestibulum egestas molestie enim, luctus interdum ante finibus vitae. Integer sagittis auctor dui vel viverra. Aliquam tristique cursus ante, et maximus ligula viverra sed. Etiam sit amet massa eget lectus dapibus maximus. Suspendisse dolor purus</p>
-			</div>
-			<div className="faq-line"></div>
-		</div>
+			<Toggle title="Daily schedule">
+					<div className="answer">
+						<p>Donec ultricies nisi justo, at auctor massa</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac odio congue, aliquet lectus id, pulvinar ex. Vestibulum egestas molestie enim, luctus interdum ante finibus vitae. Integer sagittis auctor dui vel viverra. Aliquam tristique cursus ante, et maximus ligula viverra sed. Etiam sit amet massa eget lectus dapibus maximus. Suspendisse dolor purus</p>
+					</div>
+			</Toggle>
 
-		<div className="question">
-			<h4>Different payment methods</h4>
-			<div className="answer">
-				<p>Quisque nec hendrerit neque, iaculis</p>
-				<p>Sed non velit ut odio vulputate suscipit eu ornare urna. Sed sodales nec mauris eget porttitor. Curabitur id tempus dui, a rhoncus risus. Ut pellentesque, lacus eu suscipit cursus, ante leo iaculis mauris, in maximus ante ante nec risus. Fusce imperdiet erat elementum dictum commodo. Vivamus suscipit nisl vitae </p>
-			</div>
-			<div className="faq-line"></div>
-		</div>
+			<Toggle title="Different payment methods">
+					<div className="answer">
+						<p>Quisque nec hendrerit neque, iaculis</p>
+						<p>Sed non velit ut odio vulputate suscipit eu ornare urna. Sed sodales nec mauris eget porttitor. Curabitur id tempus dui, a rhoncus risus. Ut pellentesque, lacus eu suscipit cursus, ante leo iaculis mauris, in maximus ante ante nec risus. Fusce imperdiet erat elementum dictum commodo. Vivamus suscipit nisl vitae </p>
+					</div>
+			</Toggle>
 
-		<div className="question">
-			<h4>What products do you offer</h4>
-			<div className="answer">
-				<p>Curabitur eget dignissim turpis, et pellentesque lorem.</p>
-				<p>Nullam sit amet ornare turpis, fermentum scelerisque risus. Mauris eu sem at tortor porttitor molestie. Fusce eu eleifend risus. Morbi </p>
-			</div>
-			<div className="faq-line"></div>
-		</div>
+			<Toggle title="What products do you offer ?"> 
+					<div className="answer">
+						<p>Curabitur eget dignissim turpis, et pellentesque lorem.</p>
+						<p>Nullam sit amet ornare turpis, fermentum scelerisque risus. Mauris eu sem at tortor porttitor molestie. Fusce eu eleifend risus. Morbi </p>
+					</div>
+			</Toggle>
+		</LayoutGroup>
 	</Faq>
   )
 }
@@ -64,10 +61,6 @@ const Faq = styled(BasicLayout)`
 		width: 100%;
 	}
 
-	.question{
-		padding: 3rem 0rem;
-		cursor: pointer;
-	}
 
 	.answer{
 		padding: 2rem 0rem;
