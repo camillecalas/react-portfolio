05c60b1c1f5a4ea70777590994dd2a4bb0a745ad
camille <camillecalas@hotmail.fr>
2024-05-02 14:31:50 +0200

    mobile friendly

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -99,6 +99,10 @@ const StyledAwards = styled.div`
 	margin: 5rem 10rem;
 	align-items: center;
 	justify-content: space-around;
+	@media (max-width: 1300px){
+		display: block;
+		margin: 2rem 2rem;
+	}
 `
 
 const SingleAward = styled.div`
